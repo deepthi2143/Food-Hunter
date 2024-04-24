d969af33ab7d26d2c49714f4a47b9b90bbacb3ae
Deepu@7143 <deepthiarugula05@gmail.com>
2024-04-24 00:28:52 -0700

    first commit

---
+++ fooditem.js
@@ -0,0 +1,469 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Pot Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price:  250,
+    img: 'images/biryani/pot-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 257,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Egg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 200,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Fry Piece Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 270,
+    img: 'images/biryani/fry-piece biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mutton Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 300,
+    img: 'images/biryani/hyd-Mutton-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Bone Less Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 270,
+    img: 'images/biryani/boneless.jpeg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Nalli Gosh Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 350,
+    img: 'images/biryani/nalligosh.jpeg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Bamboo Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 250,
+    img: 'images/biryani/bamboo.jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 255,
+    img: 'images/biryani/mughlai-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Grill Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 220,
+    img: 'images/chicken/grill.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chicken Curry',
+    category : 'chicken',
+    rating : 4.3,
+    price: 240,
+    img: 'images/chicken/curry.jpeg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Butter Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 240,
+    img: 'images/chicken/butter.jpeg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Noti Kodi',
+    category : 'chicken',
+    rating : 4.3,
+    price: 250,
+    img: 'images/chicken/natukodi.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Chicken Tikka',
+    category : 'chicken',
+    rating : 4.3,
+    price: 220,
+    img: 'images/chicken/tikka.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Bamboo Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 200,
+    img: 'images/chicken/bamboo.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 225,
+    img: 'images/paneer/Matar-Paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 250,
+    img: 'images/paneer/palak-paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Paneer Butter Masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 270,
+    img: 'images/paneer/paneer-butter-masala.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Malai Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 290,
+    img: 'images/paneer/malai.jpeg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Hyderabadi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 230,
+    img: 'images/paneer/Paneer-Hyderabadi.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Kadai Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 225,
+    img: 'images/paneer/kadai.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Kaaju Paneer',
+    age: '32',
+    category : 'paneer',
+    rating : 4.3,
+    price: 275,
+    img: 'images/paneer/kaju.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Butter Nans',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 120,
+    img: 'images/vegetable/nans.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Baby Corn',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 245,
+    img: 'images/vegetable/baby.jpeg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Veg Biryani',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 220,
+    img: 'images/vegetable/vegetable-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 200,
+    img: 'images/vegetable/vegetable-curry.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Veg Lollipop',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 195,
+    img: 'images/vegetable/veg lol.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 190,
+    img: 'images/vegetable/vegetable-masala.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Veg Pakora',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 170,
+    img: 'images/vegetable/vegetable-pakora.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Momos',
+    category : 'chinese',
+    rating : 4.3,
+    price: 80,
+    img: 'images/chinese/cabbage-momos-.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'chinese',
+    rating : 4.3,
+    price: 95,
+    img: 'images/chinese/ChickenManchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Chili Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 125,
+    img: 'images/chinese/Chili-Chicken.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Chowmein',
+    category : 'chinese',
+    rating : 4.3,
+    price: 160,
+    img: 'images/chinese/chowmin.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Spring Roll',
+    category : 'chinese',
+    rating : 4.3,
+    price: 175,
+    img: 'images/chinese/spring-rolls.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'chinese',
+    rating : 4.3,
+    price: 200,
+    img: 'images/chinese/szechuan-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Fried Rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 180,
+    img: 'images/chinese/veg-fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 90,
+    img: 'images/south indian/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 50,
+    img: 'images/south indian/idli-with-rice-flour.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 75,
+    img: 'images/south indian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Mysore Bonda',
+    category : 'south indian',
+    rating : 4.3,
+    price: 70,
+    img: 'images/south indian/mysore-bonda.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 45,
+    img: 'images/south indian/onion-uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'images/south indian/plain-dosa.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 45,
+    img: 'images/south indian/Rava-Uttapam.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 34,
+    img: 'images/south indian/sambhar-vada.jpg',
+    quantity: 1
+},
+{
+    id: 45,
+    name: 'Vanilla flavour',
+    category: 'ice-creams',
+    rating: 4.3,
+    price: 70,
+    img: 'images/ice-creams/vanella.jpeg',
+    quantity:1
+},
+{
+    id: 46,
+    name: 'chocolate flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 120,
+    img: 'images/ice-creams/choco.jpeg',
+},
+{
+    id: 47,
+    name: 'Strawberry flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 110,
+    img: 'images/ice-creams/straw.jpeg',  
+},
+{
+    id: 48,
+    name: 'ButterScoth flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 120,
+    img: 'images/ice-creams/butter.jpeg',
+},
+{
+    id: 49,
+    name: 'Oreo flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 150,
+    img: 'images/ice-creams/oreo.jpeg',
+},
+{
+    id: 50,
+    name: 'Dark Current flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 170,
+    img: 'images/ice-creams/dark.jpeg',
+},
+{
+    id: 51,
+    name: 'Mint chocolate flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 140,
+    img: 'images/ice-creams/mint.jpeg',
+},
+{
+    id: 52,
+    name: 'Matka Kulfi flavour',
+    category: 'ice-creams',
+    rating: 4.4,
+    price: 170,
+    img: 'images/ice-creams/kulfi.jpeg',
+},
+]
+
+export {foodItem};
